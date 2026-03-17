59157ea72b371c385c0f7107783fc0c847051a48
Jimmie Locklear <101599830+jimmielocklear@users.noreply.github.com>
2026-03-17 14:53:31 -0500

    Creating circleshape.py and adding ticks to main

---
+++ circleshape.py
@@ -0,0 +1,25 @@
+# From boot.dev - I did not write this.
+# I was instructed to paste it into this file for the exercise. - JL
+# Base class for game objects
+
+import pygame
+
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # must override
+        pass
+
+    def update(self, dt):
+        # must override
+        pass
--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -19,6 +22,9 @@ def main():
         screen.fill(000000)
 
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
+        
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
