d7a39b29075b1529f4db964087ed05ac73319d7b
Jimmie Locklear <101599830+jimmielocklear@users.noreply.github.com>
2026-03-19 11:03:59 -0500

    Added rotation logic and moved logic order in main.py per Boots suggestion due to triangle not freely rotating

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    my_player = player.Player(x, y)
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,21 +21,20 @@ def main():
         
         log_state()
 
-        for event in pygame.event.get():
-            pass
+        screen.fill("black")
 
-        screen.fill(000000)
 
-        x = SCREEN_WIDTH / 2
-        y = SCREEN_HEIGHT / 2
 
-        my_player = player.Player(x, y, PLAYER_RADIUS)
+        dt = game_clock.tick(60) / 1000
 
+
+
+        my_player.update(dt)
         my_player.draw(screen)
 
         pygame.display.flip()
 
-        dt = game_clock.tick(60) / 1000
+
         
 
         for event in pygame.event.get():
